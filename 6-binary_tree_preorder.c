0c8353561d871386321d15c1a08ec9efaab20b53
Fabian Andres <f4bian.andres@gmail.com>
2020-07-23 03:15:34 +0000

    Answer 6 binary tree preorder

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - function that send tree num to print
+ *
+ * @tree: tree root to print
+ * @func: address function
+ * Return: void
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	(*func)(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+
+}
